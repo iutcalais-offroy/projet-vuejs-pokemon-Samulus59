8d2950fa9deee8608a070ea4a1ae432a2ac41ccf
SamuelLrg <151573504+Samulus59@users.noreply.github.com>
2025-03-17 09:25:58 +0100

    Ajout inscription
    Ajout de la communication avec l'API pour l'inscription.

--- src/pages/LoginForm.page.vue
+++ src/pages/LoginForm.page.vue
@@ -1,4 +1,36 @@
-<script>
+<script setup>
+
+import { ref } from "vue";
+import axios from "axios";
+
+const email_inscription = ref("");
+const mdp_inscription = ref("");
+const verif_mdp = ref("");
+
+
+const inscription = async () => 
+{
+  if (mdp_inscription.value !== verif_mdp.value) 
+  {
+    console.error("Les mots de passe ne correspondent pas.");
+    return;
+  }
+
+  try 
+  {
+    const response = await axios.post("https://pokemon-api-seyrinian-production.up.railway.app/users", 
+    {
+      email: email_inscription.value,
+      password: mdp_inscription.value,
+    });
+
+    console.log("Inscription réussie", response.data);
+  } 
+  catch (error) 
+  {
+    console.error("Erreur lors de l'inscription:", error);
+  }
+};
 </script>
 
 <template>
@@ -14,10 +46,10 @@
       <n-tab-pane name="signin" tab="Connexion">
         <n-form>
           <n-form-item-row label="Email">
-            <n-input placeholder="Entrez votre email" />
+            <n-input name="email_connexion" placeholder="Entrez votre email" />
           </n-form-item-row>
           <n-form-item-row label="Mot de passe">
-            <n-input placeholder="Entrez votre mot de passe" />
+            <n-input name="mdp_connexion" placeholder="Entrez votre mot de passe" />
           </n-form-item-row>
         </n-form>
         <n-button type="primary" block secondary strong>
@@ -27,16 +59,16 @@
       <n-tab-pane name="signup" tab="Inscription">
         <n-form>
           <n-form-item-row label="Email">
-            <n-input placeholder="Entrez votre email" />
+            <n-input v-model="email_inscription" placeholder="Entrez votre email" />
           </n-form-item-row>
           <n-form-item-row label="Mot de passe">
-            <n-input placeholder="Entrez votre mot de passe" />
+            <n-input type="password" v-model="mdp_inscription" placeholder="Entrez votre mot de passe" />
           </n-form-item-row>
           <n-form-item-row label="Confirmer mot de passe">
-            <n-input placeholder="Confirmez votre mot de passe" />
+            <n-input type="password" v-model="verif_mdp" placeholder="Confirmez votre mot de passe" />
           </n-form-item-row>
         </n-form>
-        <n-button type="primary" block secondary strong>
+        <n-button type="primary" block secondary strong @click="inscription">
           Sign up
         </n-button>
       </n-tab-pane>
